217587cac5c604d5f04a3444c487ca4bb8d15d64
e88626 <descobar@atb.com>
2019-11-24 16:13:54 -0700

    sequelize - feat: users and notes sql tables

--- pgdb/init.sql
+++ pgdb/init.sql
@@ -1,7 +1,19 @@
 -- creates a schema for our DB
 CREATE SCHEMA notes;
 
-CREATE TABLE notes.values (
+CREATE TABLE notes.users (
+    id serial,
+    email VARCHAR(100) NOT NULL,
     number INTEGER NOT NULL,
-    CONSTRAINT PK_values PRIMARY KEY (number)
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE notes.notes (
+    id serial,
+    user int NOT NULL,
+    subject VARCHAR(250) NOT NULL,
+    note VARCHAR(1000) NOT NULL,
+    added_date timestamp NOT NULL,
+    FOREIGN KEY (user) REFERENCES notes.users(id) ON DELETE CASCADE,
+    PRIMARY KEY (id)
 );
