17a12972d4b26d954b4f250256091bd1b663803a
Dan <escobard90@gmail.com>
2019-12-02 05:21:20 -0700

    ui - refact: updated component module structure

--- ui/src/components/Navigation/index.js
+++ ui/src/components/Navigation/index.js
@@ -1,16 +1,1 @@
-import React from "react";
-
-import "./styles.scss"
-
-// needs to all sections as anchor links
-// look for css highlight options when section is selected
-const Navigation = () => (
-  <nav id="navigation">
-    <i aria-hidden="true" className="bars big icon"></i>
-    <h1>Your <br/>
-     Notes</h1>
-    <p>Nov 5, 2019</p>
-  </nav>
-);
-
-export default Navigation;
+export { default } from "./Navigation";
