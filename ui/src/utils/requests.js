65f4d7f6c37a559af14b33b1580ab0e5c3e9e33b
escbard <828$Daniel1234>
2019-12-13 11:28:43 -0700

    ui - feat: getNotes request

--- ui/src/utils/requests.js
+++ ui/src/utils/requests.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { apiRoutes, headers } from "../constants";
 
-// TODO - add commentary
+
+/** Cleans up API errors OR API promise rejections
+ * @dev refer to /utils/validation in the API
+ * @name cleanError
+ * @return clean error message
+ **/
+
 const cleanError = error => {
   let errors;
   let status;
@@ -35,4 +41,21 @@ export const addNote = async request => {
     .catch(error => {
       return cleanError(error);
     });
+};
+
+/** Sends GET request to API to getNotes
+ * @dev refer to the /getNotes route within the API request handling logic
+ * @name getNotes
+ * @returns resolved promise || rejected promise
+ **/
+
+export const getNotes = async request => {
+  return await axios
+      .get(apiRoutes.getNotes,{ headers })
+      .then(response => {
+        return response;
+      })
+      .catch(error => {
+        return cleanError(error);
+      });
 };
