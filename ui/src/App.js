fe14b5228a0356dfcae75c11932a7e2d251cadd2
Dan <escobard90@gmail.com>
2020-01-24 12:25:07 -0700

    ui-hooks-refactor - feat: App.js to hooks working

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,11 +7,11 @@ import Note from "./components/Note";
 import Modal from "./components/Modal";
 
 import { addNoteFields } from "./constants";
-import { addNote, getNotes } from "./utils/requests";
+import { addNote as addNoteRequest, getNotes } from "./utils/requests";
 
 import "./styles/global.scss";
 
-const AppNew = () => {
+const App = () => {
   const id = "application";
 
   const [messageErrors, setMessageErrors] = useState([]),
@@ -36,6 +36,7 @@ const AppNew = () => {
   useEffect(async () => {
     const notes = await getNotes();
     setNotes(notes);
+    console.log("NOTES INIT", notes);
   }, []);
 
   /** Submits the POST request to the API
@@ -47,6 +48,7 @@ const AppNew = () => {
    **/
 
   const addNote = async (subject, note) => {
+    console.log("NOTE ADDNOTE", note);
     // triggers validation logic
     validateAddNote(subject, note);
 
@@ -57,7 +59,7 @@ const AppNew = () => {
         note
       };
 
-      let response = await addNote(request);
+      let response = await addNoteRequest(request);
 
       // checks for API promise rejections
       if (!response.status) {
@@ -71,7 +73,7 @@ const AppNew = () => {
 
         setTitle("addNote() added!");
         setMessage(status);
-        setStatus("red");
+        setStatus("green");
 
         const notes = await getNotes();
 
@@ -79,7 +81,7 @@ const AppNew = () => {
         setTimeout(async () => {
           setNotes(notes);
           setShow(false);
-          setStatus(false);
+         return  setStatus(false);
         }, 500);
       }
     }
@@ -91,9 +93,11 @@ const AppNew = () => {
    * @param {string} error, string of error to add to this.state.message
    **/
 
-  const validateField = (condition, error) => {
+  const validateField = (value, condition, error) => {
     if (condition) {
-      this.setState({ messageErrors: this.state.messageErrors.push(error) });
+      const errors = messageErrors;
+      errors.push(error);
+      setMessageErrors(errors);
     }
   };
 
@@ -105,7 +109,9 @@ const AppNew = () => {
    **/
 
   const validateAddNote = (subject, note) => {
+    console.log("NOTE", note);
     validateField(
+      note,
       note.length < 25,
       "Note must contain more than 25 characters"
     );
@@ -141,7 +147,7 @@ const AppNew = () => {
       return <Note key={id + index} id={`${id}-${index}`} data={object} />;
     });
   };
-
+  console.log("RENDERED");
   return (
     <Fragment>
       <Header />
@@ -151,206 +157,21 @@ const AppNew = () => {
           id={id}
           title="Add Note"
           open={show}
-          close={this.close}
+          close={() => (setShow(false), setStatus(false))}
           content={
             <Form
               id={id}
               message={formMessage}
-              addNote={this.addNote}
+              addNote={addNote}
               fields={addNoteFields}
             />
           }
         />
-        {this.renderNotes(id, notes)}
-        <Footer id={id} open={this.open()} />
+        {renderNotes(id, notes)}
+        <Footer id={id} open={() => setShow(true)} />
       </main>
     </Fragment>
   );
 };
-
-class App extends Component {
-  state = {
-    messageErrors: [],
-    title: "Post Form",
-    message:
-      "Follow the placeholder instructions to validate data on the UI and API side",
-    status: false,
-    show: false,
-    notes: []
-  };
-
-  async componentDidMount() {
-    const notes = await getNotes();
-    this.setState({ notes });
-  }
-
-  open = size => () => this.setState({ size, show: true });
-  close = () => this.setState({ show: false, status: false });
-
-  /** Submits the POST request to the API
-   * @name addNote
-   * @dev this requests tests basic validation between UI and API
-   * @param {string} subject, contains note's subject value
-   * @param {string} note, contains note's note value
-   * @returns /addNote route response, or validation message
-   **/
-
-  addNote = async (subject, note) => {
-    let { messageErrors } = this.state;
-
-    // triggers validation logic
-    this.validateAddNote(subject, note);
-
-    // only runs request, if no validation message are present
-    if (messageErrors.length === 0) {
-      const request = {
-        subject,
-        note
-      };
-
-      let response = await addNote(request);
-
-      // checks for API promise rejections
-      if (!response.status) {
-        return this.setState({
-          title: "addNote() error(s)",
-          message: response,
-          status: "red"
-        });
-      } else if (response.data.note) {
-        const {
-          data: { status }
-        } = response;
-
-        this.setState({
-          title: "addNote() added!",
-          message: status,
-          status: "green"
-        });
-
-        const notes = await getNotes();
-
-        // add timeout here to close out modal on note creation
-        setTimeout(async () => {
-          this.setState({ notes });
-          this.close();
-        }, 500);
-      }
-    }
-  };
-
-  /** Resets the message array after form validation checks
-   * @returns this.setState()
-   **/
-
-  emptyErrors = () => {
-    this.setState({
-      messageErrors: []
-    });
-  };
-
-  /** Validates a form value
-   * @dev can be split out into a validation class to re-use in api / ui layers
-   * @param {*} value, property to validate
-   * @param {*} condition, functional condition to validate / invalidate value
-   * @param {string} error, string of error to add to this.state.message
-   **/
-
-  validateField = (value, condition, error) => {
-    if (condition) {
-      this.setState({ messageErrors: this.state.messageErrors.push(error) });
-    }
-  };
-
-  /** Validates addNote values
-   * @name validateAddNote
-   * @dev used to reduce clutter in makeDonation
-   * @param {string} subject, contains random string value
-   * @param {string} note, contains random string value with a length greater than 10
-   **/
-
-  validateAddNote = (subject, note) => {
-    let { messageErrors } = this.state;
-
-    this.validateField(
-      note,
-      note.length < 25,
-      "Note must contain more than 25 characters"
-    );
-
-    // sets messagesState
-    if (messageErrors.length > 0) {
-      this.setState({
-        status: "red",
-        title: "addNote() form error:",
-        message: `Form contains the following error: ${messageErrors.join(
-          ", "
-        )}.`
-      });
-      this.emptyErrors();
-    } else {
-      this.setState({
-        status: "green",
-        title: "addNote() validated",
-        message: `Adding note...`
-      });
-    }
-  };
-
-  /** Renders Notes based on API response
-   * @name renderNotes
-   * @dev used to render multiple notes
-   * @param {array} data, contains random string value
-   * @returns one or more <Note />
-   **/
-
-  renderNotes(id, data) {
-    if (data.length === 0) {
-      return <p>No notes</p>;
-    }
-    return data.map((object, index) => {
-      return <Note key={id + index} id={`${id}-${index}`} data={object} />;
-    });
-  }
-
-  render() {
-    const id = "application";
-
-    let { title, message, status, show, notes } = this.state;
-
-    const formMessage = status
-      ? {
-          color: status,
-          header: title,
-          content: message
-        }
-      : null;
-
-    return (
-      <Fragment>
-        <Header />
-        <div className="divider" />
-        <main id={id} className="application">
-          <Modal
-            id={id}
-            title="Add Note"
-            open={show}
-            close={this.close}
-            content={
-              <Form
-                id={id}
-                message={formMessage}
-                addNote={this.addNote}
-                fields={addNoteFields}
-              />
-            }
-          />
-          {this.renderNotes(id, notes)}
-          <Footer id={id} open={this.open()} />
-        </main>
-      </Fragment>
-    );
-  }
-}
 
 export default App;
