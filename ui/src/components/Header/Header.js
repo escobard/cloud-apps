fed930ce9a69774939c7355be4ca927661ccc30b
Dan <escobard90@gmail.com>
2020-02-23 19:35:13 -0700

    ui-header-tests - feat: improved header semantics

--- ui/src/components/Header/Header.js
+++ ui/src/components/Header/Header.js
@@ -12,8 +12,7 @@ const Header = ({ id }) => (
   <nav id={`${id}-navigation`}>
     <i aria-hidden="true" className="bars big icon" />
     <h1>
-      Your <br />
-      Notes
+      Your Notes
     </h1>
     <p>Nov 5, 2019</p>
   </nav>
