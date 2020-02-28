b2a9011358579997d6e6e98027f7bffe39020fca
Dan <escobard90@gmail.com>
2020-02-27 19:35:20 -0700

    api-swagger-upgrade - feat: base middleware setupp

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+  createMiddleware = require("swagger-express-middleware"),
   bodyParser = require("body-parser"),
   cors = require("cors"),
   app = express(),
@@ -10,8 +11,19 @@ app.use(cors({ origin: "*" }));
 
 require("./routes")(app);
 
-let server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+let server = createMiddleware("PetStore.yaml", app, (err, middleware) => {
+  // Add all the Swagger Express Middleware, or just the ones you need.
+  // NOTE: Some of these accept optional options (omitted here for brevity)
+  app.use(
+    middleware.metadata(),
+    middleware.CORS(),
+    middleware.parseRequest(),
+    middleware.validateRequest(),
+  );
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
+});
 
 module.exports = server;
