5f88a63bec47dfaae5f01f61d7635d60de5652fc
Dan <escobard90@gmail.com>
2020-02-29 16:39:51 -0700

    api-swagger-upgrade - feat: updated api to not crash on unavailable DB

--- api/services/postgres/index.js
+++ api/services/postgres/index.js
@@ -1,57 +1,57 @@
-const Sequelize = require('sequelize'),
+const Sequelize = require("sequelize"),
   {
     pgPort,
     pgHost,
     pgUser,
     pgDatabase,
     pgPassword
-  } = require('../../constants/dbkeys');
+  } = require("../../constants/dbkeys");
 
-const sequelize = new Sequelize(
-  pgDatabase,
-  pgUser,
-  pgPassword,
-  {
-    host: pgHost,
-    port: pgPort,
-    dialect: 'postgres',
-    define: {
-      timestamps: false,
-      schema: "notes"
-    }
-  },
-);
+const sequelize = new Sequelize(pgDatabase, pgUser, pgPassword, {
+  host: pgHost,
+  port: pgPort,
+  dialect: "postgres",
+  define: {
+    timestamps: false,
+    schema: "notes"
+  }
+});
 
 const models = {
-  Users: sequelize.import('./models/users'),
-  Notes: sequelize.import('./models/notes')
+  Users: sequelize.import("./models/users"),
+  Notes: sequelize.import("./models/notes")
 };
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    global.hasDB = true;
+    console.log("Connection has been established successfully.");
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
+    global.hasDB = false;
   });
 
 const { Users } = models;
+Users.findAll()
+  .then(values => {
+    const { hasDB } = global;
 
-Users.findAll().then(values => {
-  
-  const cleanData = JSON.stringify(values, null, 4)
-  
-  console.log("All users:", cleanData);
-  if(values.length === 0){
-    Users.create({ email: "admin@admin" }).then(admin => {
-      console.log("Auto-generated ID:", admin.id, admin.email);
-    });
-  }
-})
+    const cleanData = JSON.stringify(values, null, 4);
 
-module.exports.default =
-module.exports = {
+    console.log("All users:", cleanData);
+    if (hasDB === true && values.length === 0) {
+      Users.create({ email: "admin@admin" }).then(admin => {
+        console.log("Auto-generated ID:", admin.id, admin.email);
+      });
+    }
+  })
+  .catch(err => {
+    console.error("Unable to connect to create base user", err);
+  });
+
+module.exports.default = module.exports = {
   sequelize,
   models
-}
+};
