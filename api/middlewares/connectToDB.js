88dc4a6c5ec5c853ad5be42bab21b92ca3c8850b
Dan <escobard90@gmail.com>
2020-02-29 17:57:58 -0700

    api-swagger-upgrade - feat: cleanup, simplified middleware

--- api/middlewares/connectToDB.js
+++ api/middlewares/connectToDB.js
@@ -3,20 +3,9 @@ const { sequelize } = require("../services/postgres");
  * @dev prevents API crashes when DB is unavailable
  */
 
-module.exports = function(options) {
-  return (req, res, next) => {
-    return sequelize
-      .authenticate()
-      .then(() => {
-        global.hasDB = true;
-        console.log("Connection has been established successfully.");
-        next();
-      })
-      .catch(err => {
-        const error = "Unable to connect to the database: " + err;
-        console.error(error);
-        global.hasDB = false;
-        res.status(503).json(error);
-      });
-  };
-};
+module.exports = async (req, res, next) => {
+  if (global.hasDB === false){
+    return res.status(503).json(error);
+  }
+  next()
+}
