95c35c99ad60fd41d474d2584adbafad659aa16a
Dan <escobard90@gmail.com>
2020-03-08 20:26:40 -0600

    integration-framework-base - ops: removed nyc package

--- integration/tests/tests-in-order.js
+++ integration/tests/tests-in-order.js
@@ -1,5 +1,5 @@
 /** This file is used to run tests in a specific order
- * @dev CI/CD driven tests will use this file, mocha bails if any one test fails
+ * @dev CI/CD driven tests will use this file, tests stops if any one test fails
  */
 
 require("../config.test");
