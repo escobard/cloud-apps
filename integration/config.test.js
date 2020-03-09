4dd59ba7bda26a4bd6f7164b4e3a03db6d932426
Dan <escobard90@gmail.com>
2020-03-08 19:49:20 -0600

    integration-framework-base - feat: base health test

--- integration/config.test.js
+++ integration/config.test.js
@@ -1,4 +1,7 @@
 before((done) =>{
   global.request = require("supertest");
-  global.expect = require("chai").expect;
+  global.expect = require("chai").expect
+  // TODO - improve this portion during docker phase to include PORT as well
+  global.server = process.env.API_HOST ||"http://localhost:4000"
+  done()
 });
---
+++ integration/tests/health.test.js
@@ -0,0 +1,5 @@
+describe("API Health", () => {
+  it(">> has DB, is running in test environment", ()=>{
+
+  })
+})
