447d2110f2291529ca3df8ee0ff32b91e26ccb40
Dan <escobard90@gmail.com>
2020-03-08 20:02:56 -0600

    integration-framework-base - feat: health test working as expected!

--- integration/tests/health.test.js
+++ integration/tests/health.test.js
@@ -1,5 +1,16 @@
 describe("API Health", () => {
-  it(">> has DB, is running in test environment", ()=>{
+  it(">> has DB, is running in test environment", done => {
 
-  })
-})
+    // TODO - add this
+    const health = { healthy: true, DB: true, /* environment: testing */ };
+
+    request(server)
+      .get("/health")
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal(health);
+        if (err) return done(err);
+        done();
+      });
+  });
+});
