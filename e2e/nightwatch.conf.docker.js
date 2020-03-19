c6c0e3942fb948c7dbb029898b890c3917c290f9
Dan <escobard90@gmail.com>
2020-03-19 16:08:23 -0600

    e2e-docker - feat: job cleanup, not using prod api / ui until ui prod is fixed

--- e2e/nightwatch.conf.docker.js
+++ e2e/nightwatch.conf.docker.js
@@ -1,4 +1,23 @@
-module.exports = (function(settings) {
-  settings.test_workers = false;
-  return settings;
-})(require("./nightwatch.json"));
+module.exports = {
+  src_folders: ["tests"],
+  page_objects_path: ["screens"],
+  test_workers: false,
+  selenium: {
+    start_process: false
+  },
+  test_settings: {
+    default: {
+      selenium_port: 4444,
+      selenium_host: "chromedriver",
+      launch_url: process.env.UI_HOST,
+      filter: "tests/**/*test.js",
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+          args: ["headless"],
+          w3c: false
+        }
+      }
+    }
+  }
+};
