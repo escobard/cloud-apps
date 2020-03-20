167b3f75793587d756684a5179befc840543e479
Dan <escobard90@gmail.com>
2020-03-20 07:28:04 -0600

    ui-swagger-upgrade - feat: swagger response upgrade base

--- api/middlewares/swaggerValidation.js
+++ api/middlewares/swaggerValidation.js
@@ -10,10 +10,24 @@ module.exports = function swaggerValidation(err){
       res.type("application/json");
       console.log("Swagger validator error");
       console.log("Status: " + err.status);
-      // TODO - need a util to format this string to improve its readability
-      let message = err.message.split(/\r\n|\r|\n/g);
       console.log("Message: " + err.message);
-      res.json({status: err.status, description: message})
+      // TODO - need a util to format these string to improve its readability
+      let schemaPath;
+      let missingProp;
+      let fullError = err.message.split(/\r\n|\r|\n/g);
+      let message = fullError[0]
+      fullError.map((err) => {
+        if (err.includes("Schema path")){
+          console.log('SCHEMA')
+          schemaPath = err;
+        }
+        if (err.includes("Missing required")){
+          console.log('MISSING')
+          missingProp = err;
+        }
+      })
+      console.log('INCLUDES', fullError.includes("Schema") )
+      res.json({status: err.status, message, schemaPath, missingProp})
     } else {
       next();
     }
