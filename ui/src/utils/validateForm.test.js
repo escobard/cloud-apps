230132c56c1fcc2c93b4307959658b2eb6c23a21
Dan <escobard90@gmail.com>
2020-03-22 17:26:05 -0600

    ui-validation-refactor - test: validateForm unit tests complete!

--- ui/src/utils/validateForm.test.js
+++ ui/src/utils/validateForm.test.js
@@ -1,18 +1,26 @@
 import { validateForm} from "./validateForm";
 
-// TODO - look into improving this absolute path logic to match ui code
 import { addNoteFields } from "Constants";
 
-describe("validateForm", () => {
-  console.log(addNoteFields)
-  const conditions = [
+describe("validateForm util", () => {
+  const trueCondition = [
     {
       condition: true,
       error: addNoteFields[0].errors[0]
-    },
+    }
+  ];
+  const falseCondition = [
     {
       condition: false,
       error: addNoteFields[1].errors[0]
     }
-  ];
+  ]
+  it(">> pushes error to array if condition is met", () =>{
+    const results = validateForm(trueCondition)
+    expect(results).toEqual(["Subject must contain more than 5"]);
+  })
+  it(">> does not pushe error to array if condition not met", () =>{
+    const results = validateForm(falseCondition)
+    expect(results).toEqual([]);
+  })
 })
