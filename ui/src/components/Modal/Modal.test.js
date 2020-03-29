cdac2482dd384b3819cb1ed3fd1b42aa433efaef
Dan <escobard90@gmail.com>
2020-03-28 19:06:44 -0600

    ui-unit-test-cleanup - test: modal props

--- ui/src/components/Modal/Modal.test.js
+++ ui/src/components/Modal/Modal.test.js
@@ -1,8 +1,21 @@
 import Modal from "./index";
 
 describe(">> Modal component", () => {
+  const TestComponent = () => (
+    <>
+      <h3>Test</h3>
+    </>
+  );
+  const props = {
+    state: true,
+    close: jest.fn(),
+    data: {
+      title: "Test title",
+      content: <TestComponent />
+    }
+  };
   it(">> snapshot is up to date", () => {
-    const { container } = render(<Modal />);
+    const { container } = render(<Modal {...props} />);
     expect(container).toMatchSnapshot();
   });
 });
