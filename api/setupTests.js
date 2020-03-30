6abf462cac691018ed01d37d5f3ea79d16b4b076
Dan <escobard90@gmail.com>
2020-03-29 21:06:07 -0600

    api-babel-jest - feat: debugging pr

---
+++ api/setupTests.js
@@ -0,0 +1,43 @@
+/** Configuration file for tests
+ * @dev source of truth for all unit tests in the application
+ */
+
+const { routes } = require("./constants");
+
+// timeout is necessary to unit test swagger middleware
+beforeAll(async () =>{
+
+  const { health, addNote, getNotes } = routes;
+
+  // sets port to something else to avoid clashes with local dev
+  // for whatever reason, nyc fails when port changes
+  //process.env.PORT = 5555;
+
+  // setup global environment variable for server + request builder
+  global.request = require("supertest");
+  global.server = require("./index");
+  global.sinon = require("sinon");
+  global.expect = require("chai").expect;
+
+  // routes
+  global.health = health;
+  global.addNote = addNote;
+  global.getNotes = getNotes;
+
+  // TODO - expand request with external API url, if tests are running in CI/CD
+});
+
+beforeEach(async () => {
+  server = require("./index");
+});
+
+afterEach(done => {
+  sinon.restore();
+  server.close();
+  done();
+});
+
+afterAll(function(done){
+  sinon.restore();
+  done();
+});
