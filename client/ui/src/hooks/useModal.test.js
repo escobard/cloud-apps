e08ca1891eff16ea895d9d8d75436ba0079a134e
Dan <escobard90@gmail.com>
2020-07-24 15:12:20 -0600

    test: useModal TDD

--- client/ui/src/hooks/useModal.test.js
+++ client/ui/src/hooks/useModal.test.js
@@ -1,5 +1,18 @@
 import { useModal } from "hooks";
 
 describe("useModal hook", () => {
-
+  it("> opens modal", () => {
+    const { openModal } = renderHook(() => useModal());
+    act(async () => {
+      openModal();
+    });
+    expect(result.current.showModal).toEqual(true);
+  })
+  it("> closes modal", () => {
+    const { closeModal } = renderHook(() => useModal());
+    act(async () => {
+      closeModal();
+    });
+    expect(result.current.showModal).toEqual(false);
+  })
 })
